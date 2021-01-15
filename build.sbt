a5d64c88b8aa385b118adc05690a724599c3f4d8
Ali Karami <alijy@users.noreply.github.com>
2021-01-15 12:33:21 +0000

    Added the missing plugins

--- build.sbt
+++ build.sbt
@@ -1,9 +1,17 @@
+import scoverage.ScoverageKeys
 import uk.gov.hmrc.DefaultBuildSettings.integrationTestSettings
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin.publishingSettings
 
 val appName = "sole-trader-identification"
 
 val silencerVersion = "1.7.0"
+
+lazy val scoverageSettings = Seq(
+  ScoverageKeys.coverageExcludedPackages  := "<empty>;Reverse.*;config.*;.*(AuthService|BuildInfo|Routes).*",
+  ScoverageKeys.coverageMinimum           := 100,
+  ScoverageKeys.coverageFailOnMinimum     := false,
+  ScoverageKeys.coverageHighlighting      := true
+)
 
 lazy val microservice = Project(appName, file("."))
   .enablePlugins(play.sbt.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin)
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,10 +11,11 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.3.0" % Test,
-    "org.scalatest"           %% "scalatest"                % "3.2.3"  % Test,
-    "com.typesafe.play"       %% "play-test"                % current  % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.3.0"   % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.3"   % Test,
+    "com.typesafe.play"       %% "play-test"                % current   % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"  % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"   % "test, it",
+    "org.scoverage"           %% "scalac-scoverage-runtime" % "1.4.1"   % "test, it"
   )
 }
