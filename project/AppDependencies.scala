6721ce0c66cc190d0077642ced43b4ad3c0db7bc
Milly Harrison <64021464+HarrNM@users.noreply.github.com>
2022-08-26 10:02:01 +0100

    SAR-10318: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                        %% "bootstrap-backend-play-28"  % "5.16.0",
-    "uk.gov.hmrc.mongo"                  %% "hmrc-mongo-play-28"         % "0.68.0"
+    "uk.gov.hmrc"                        %% "bootstrap-backend-play-28"  % "7.1.0",
+    "uk.gov.hmrc.mongo"                  %% "hmrc-mongo-play-28"         % "0.71.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                        %% "bootstrap-test-play-28"     % "5.16.0"  % Test,
+    "uk.gov.hmrc"                        %% "bootstrap-test-play-28"     % "7.1.0"  % Test,
     "org.scalatest"                      %% "scalatest"                  % "3.2.9"   % "test, it",
     "com.typesafe.play"                  %% "play-test"                  % current   % Test,
     "com.vladsch.flexmark"               %  "flexmark-all"               % "0.36.8"  % "test, it",
@@ -19,6 +19,6 @@ object AppDependencies {
     "org.mockito"                        %% "mockito-scala"              % "1.16.37" % Test,
     "org.mockito"                        %% "mockito-scala-scalatest"    % "1.16.37" % Test,
     "com.fasterxml.jackson.module"       %% "jackson-module-scala"       % "2.12.2"  % "test, it",
-    "uk.gov.hmrc.mongo"                  %% "hmrc-mongo-test-play-28"    % "0.68.0"  % Test
+    "uk.gov.hmrc.mongo"                  %% "hmrc-mongo-test-play-28"    % "0.71.0"  % Test
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,9 +3,9 @@ resolvers += MavenRepository("HMRC-open-artefacts-maven2", "https://open.artefac
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 resolvers += Resolver.typesafeRepo("releases")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.4.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.8.0")
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.8")
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.7.3")
-addSbtPlugin("uk.gov.hmrc" % "sbt-bobby" % "3.5.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-bobby" % "4.1.0")
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
