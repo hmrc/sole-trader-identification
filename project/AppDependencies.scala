26d3e18104bae12a142940b0ce349651db8d1f5b
DarylMConway <daryl.conway@digital.hmrc.gov.uk>
2026-01-07 12:18:01 +0000

    VER-4832 Scala 3 update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootStrapVersion: String = "10.4.0"
+  val bootStrapVersion: String = "10.5.0"
   val mongoVersion: String = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -13,15 +13,15 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"    % bootStrapVersion  % Test,
-    "org.scalatest"                %% "scalatest"                 % "3.2.19"          % Test,
-    "org.playframework"            %% "play-test"                 % current           % Test,
-    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.64.8"          % Test,
-    "org.mockito"                   % "mockito-core"              % "5.21.0"          % Test,
-    "org.scalatestplus.play"       %% "scalatestplus-play"        % "7.0.2"           % Test,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.20.1"          % Test,
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30"   % mongoVersion      % Test
-  )
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"    % bootStrapVersion,
+    "org.scalatest"                %% "scalatest"                 % "3.2.19",
+    "org.playframework"            %% "play-test"                 % current,
+    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.64.8",
+    "org.mockito"                   % "mockito-core"              % "5.21.0",
+    "org.scalatestplus.play"       %% "scalatestplus-play"        % "7.0.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.20.1",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-30"   % mongoVersion
+  ).map(_ % Test)
 
   val it: Seq[ModuleID] = Seq(
     "org.wiremock"                 % "wiremock"                   % "3.13.2"           % Test
